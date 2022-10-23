5d27752f9f282da6669c23771c101165bcedc438
BozWorld <mondohamoeva@hotmail.fr>
2022-10-23 19:39:46 +0200

    Add Test

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -8,7 +8,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(boz);
     }
 
     // Update is called once per frame
