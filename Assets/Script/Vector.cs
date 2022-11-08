5c2bf660be35ff36a1f372ea0a610f0afaca4567
BozWorld <mondohamoeva@hotmail.fr>
2022-11-08 09:48:12 +0100

    Finish Exercice 2

--- Assets/Script/Vector.cs
+++ Assets/Script/Vector.cs
@@ -4,65 +4,95 @@ using UnityEngine;
 
 public class Vector : MonoBehaviour
 {
-    [Range(0,10f)]
-    public float radius = 1f;
+    [Range(0, 10f)]
+    public float Radius = 1f;
 
-    [Range(0,10f)]
-    public float unit = 1f;
+    [Range(0, 1f)]
+    public float Threshold;
 
-    public GameObject pointB;
+    [Range(0, 10f)]
+    public float Unit = 1f;
 
-    private void OnDrawGizmos() {
+    private float dotV;
 
-        Gizmos.DrawLine( Vector2.zero , transform.position.normalized);
-        float length = transform.position.magnitude;
-        float distance = Vector2.Distance(transform.position,pointB.transform.position);
-        Vector2 dir = (pointB.transform.position - transform.position).normalized;
-        //Gizmos.DrawLine ( Vector2.zero, Vector3.right  * distance);
-        Gizmos.DrawLine ( transform.position, (Vector2)transform.position + dir);
+    public GameObject PointB;
 
-        Vector2 offsetVec = dir* unit;
-        
-        Gizmos.DrawSphere ( (Vector2)transform.position + offsetVec , 0.05f);
-
+    private void OnDrawGizmos()
+    {
+        //Exercice1();
+        Exercice2();
     }
 
-/*     private void exercice1(){
+    private void pointAlongDirection()
+    {
+        Gizmos.DrawLine(Vector2.zero, transform.position.normalized);
+        float length = transform.position.magnitude;
+        float distance = Vector2.Distance(transform.position, PointB.transform.position);
+        Vector2 dir = (PointB.transform.position - transform.position).normalized;
+        //Gizmos.DrawLine ( Vector2.zero, Vector3.right  * distance);
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + dir);
+
+        Vector2 offsetVec = dir * Unit;
+
+        Gizmos.DrawSphere((Vector2)transform.position + offsetVec, 0.05f);
+    }
+
+    private void Exercice1()
+    {
 
         Vector3 center = transform.position;
 
-        if ( player == null ) {
+        if (PointB == null)
+        {
             return;
         }
 
-        Vector3 playerPos = player.transform.position;
+        Vector3 PointBPos = PointB.transform.position;
 
-        float dist = Vector3.Distance (center, playerPos);
+        float dist = Vector3.Distance(center, PointBPos);
 
-        bool inside = dist < radius;
+        bool inside = dist < Radius;
 
         Gizmos.color = inside ? Color.green : Color.red;
 
-        Gizmos.DrawWireSphere ( center , radius);
-    } */
-
-/*     private void exercice2()
+        Gizmos.DrawWireSphere(center, Radius);
+    }
+    private void Exercice2()
     {
+        Vector2 dir = PointB.transform.position - transform.position;
+        dir = dir.normalized;
+
+        Vector2 lookAt = transform.right;
+
+        float dot = Vector2.Dot(dir, lookAt);
+        dotV = dot;
+        bool isLooking = dot >= Threshold;
+
+        Gizmos.color = isLooking ? Color.green : Color.red;
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + dir);
+
         Gizmos.color = Color.white;
-        Vector3 direction = transform.TransformDirection(Vector3.right)*unit;
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + lookAt);
 
-        RaycastHit hit;
-        
-        if(Physics.Raycast(transform.position,direction, out hit, unit)){
-            Gizmos.color = Color.red;
-            //Vector3.Dot(dir, hit.normal);
-        }
-        Gizmos.DrawRay(transform.position,direction);
-    } */
 
-/*     private void exercice3 () {
-        Vector2 objPos = transform.position;
-        Vector2 right = transform.right;
-        Vector2 up = transform.up;
-    } */
+    }
+    /*     private void Exercice2()
+        {
+            Gizmos.color = Color.white;
+            Vector3 direction = transform.TransformDirection(Vector3.right)*Unit;
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position,direction, out hit, Unit)){
+                Gizmos.color = Color.red;
+                //Vector3.Dot(dir, hit.normal);
+            }
+            Gizmos.DrawRay(transform.position,direction);
+        } */
+
+    /*     private void Exercice3 () {
+            Vector2 objPos = transform.position;
+            Vector2 right = transform.right;
+            Vector2 up = transform.up;
+        } */
 }
